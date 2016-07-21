17217f31e14fad65bdf5aa1dda8fa8b03c6a3f21
Philipp_Kozlov <Philipp_Kozlov@EPBYMINW3256>
2016-07-21 18:21:14 +0300

    Added HashTableTest.

---
+++ Day7/ConstTask/TaskConsole/HashTableTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashTable = new Hashtable();
+            hashTable.Add(1, "A");
+            hashTable.Add(2, new object());
+            hashTable.Add(3, 123);
+
+            foreach (var key in hashTable.Keys)
+            {
+                Console.WriteLine($"{hashTable[key]} - {hashTable[key].GetType()}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
