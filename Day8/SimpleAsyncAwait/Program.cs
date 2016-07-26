c9a53de6622f90e3c7416a153025ecd8f6d6f464
Philipp_Kozlov <Philipp_Kozlov@EPBYMINW3256>
2016-07-26 19:27:58 +0300

    Changed DoSomeWork to work in async mode.

--- Day8/SimpleAsyncAwait/Program.cs
+++ Day8/SimpleAsyncAwait/Program.cs
@@ -21,10 +21,11 @@ namespace SimpleAsyncAwait
         {
             Console.WriteLine("Waiting for task to complete.");
             // TODO: AsyncHelper.RunSync(DoSomeWork);
+            AsyncHelper.RunSync(() => Task.Run(() => DoSomeWork()));
             Console.WriteLine("Task is completed. Press any key to continue.");
         }
 
-        private static void DoSomeWork()
+        private async static void DoSomeWork()
         {
             Console.WriteLine("Press any key to do the work.");
             Console.ReadKey();
@@ -35,9 +36,9 @@ namespace SimpleAsyncAwait
             WebClient webClient = new WebClient();
             stopwatch.Start();
             // TODO: use async/await here to run those tasks in asynchronous style.
-            webClient.DownloadString(string.Format(searchRequest, "pokemon"));
-            webClient.DownloadString(string.Format(searchRequest, "epam"));
-            webClient.DownloadString(string.Format(searchRequest, "minsk"));
+            var result1 = await webClient.DownloadStringTaskAsync(string.Format(searchRequest, "pokemon"));
+            var result2 = await webClient.DownloadStringTaskAsync(string.Format(searchRequest, "pokemon"));
+            var result3 = await webClient.DownloadStringTaskAsync(string.Format(searchRequest, "minsk"));
             stopwatch.Stop();
             Console.WriteLine(string.Format("Total time is {0}ms.", stopwatch.ElapsedMilliseconds));
         }
